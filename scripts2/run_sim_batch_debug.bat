b1afdc8f22168e7d5f9f0f5d39c9f727922dd01b
thekingofhero <thekingofhero@aliyun.com>
2015-11-11 13:46:46 +0800

    delete

--- scripts2/run_sim_batch_debug.bat
+++
@@ -1,7 +0,0 @@
-RMDIR /S /Q configs
-
-SET LOG="sim_batch_debug.log"
-
-IF EXIST %LOG% DEL %LOG%
-
-sim_batch_debug.bat >> %LOG%
--- scripts2/run_sim_batch_release.bat
+++
@@ -1,7 +0,0 @@
-RMDIR /S /Q configs
-
-SET LOG="sim_batch_release.log"
-
-IF EXIST %LOG% DEL %LOG%
-
-sim_batch_release.bat >> %LOG%
